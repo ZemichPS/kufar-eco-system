04771a3870fe7bd193ecd54333feaa1581dce5bd
Zemich <compserv@yahoo.com>
2025-04-20 00:41:25 +0300

    save next step

--- user-service/src/main/java/by/zemich/userservice/infrastructure/security/LoginRequest.java
+++ user-service/src/main/java/by/zemich/userservice/infrastructure/security/LoginRequest.java
@@ -1,9 +1,11 @@
 package by.zemich.userservice.infrastructure.security;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @Email
     private String email;
     private String password;
 }
