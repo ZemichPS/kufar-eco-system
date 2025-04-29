f506266311d2677e8de6c4574cfcd31d6b082931
User <compserv@yahoo.com>
2025-04-29 09:07:39 +0300

    add catalog device

---
+++ device-catalog-service/src/main/java/by/zemich/device_catalog_service/DeviceCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package by.zemich.device_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ device-catalog-service/src/test/java/by/zemich/device_catalog_service/DeviceCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zemich.device_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
