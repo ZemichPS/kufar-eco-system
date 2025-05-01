34293cf6017ef6a9894465c0cab53ac8c276ac03
Zemich <compserv@yahoo.com>
2025-05-01 20:19:08 +0300

    create geos service

---
+++ geo-service/src/test/java/by/zemich/geo_service/GeoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zemich.geo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
